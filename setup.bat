1956bc01335650cc4a451d499932ecfc9aafef85
Viccazi <115668817+Viccazi@users.noreply.github.com>
2025-03-20 12:45:18 +0530

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,34 @@
+@echo off
+title Bettor Whale Bot by @MeoMunDep
+color 0A
+
+cd %~dp0
+
+echo Checking configuration files...
+
+(for %%F in (datas.txt proxies.txt) do (
+    if not exist %%F (
+        type nul > %%F
+        echo Created %%F
+    )
+))
+
+echo Configuration files checked.
+
+echo Checking dependencies...
+if exist "..\node_modules" (
+    echo Using node_modules from parent directory...
+    cd ..
+    CALL npm install user-agents axios colors https-proxy-agent socks-proxy-agent 
+    cd %~dp0
+) else (
+    echo Installing dependencies in current directory...
+    CALL npm install user-agents axios colors https-proxy-agent socks-proxy-agent 
+)
+echo Dependencies installation completed!
+
+echo Starting the bot...
+node meomundep
+
+pause
+exit
